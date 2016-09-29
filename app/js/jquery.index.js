8513c5bd01ea74d1002aee18af591b9cc66a4ca4
petruninevgeniy <petruninevgeniy@gmail.com>
2016-09-29 14:25:54 +0300

    add hover to pagination btn

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -129,38 +129,6 @@
                     }
                 } );
 
-                // paginationCustomRender: function( swiper, current, total ) {
-                //
-                //     var names = [];
-                //
-                //     $( '.swiper-wrapper .swiper-slide' ).each( function(i) {
-                //
-                //         names.push( $(this).data('name') );
-                //
-                //     } );
-                //
-                //     var text = '<div class="swiper-pagination__text">';
-                //
-                //     for ( var i = 1; i <= total; i++ ) {
-                //
-                //         if ( current == i ) {
-                //
-                //             text += '<span class="active"><span>' + names[i] + '</span></span>';
-                //
-                //         } else {
-                //
-                //             text += '<span><span>' + names[i] + '</span></span>';
-                //
-                //         }
-                //
-                //     }
-                //
-                //     text += '</div>';
-                //
-                //     return text;
-                //
-                // }
-
                 _slides = _obj.find( '.main-slider__slide');
 
             },
